c6bd515ab8fd1e0583b0e599ccc1bd2148073aaf
Rajnish Gahlauth <rraj81026@gmail.com>
2025-12-17 14:47:37 +0000

    shift explore and timeline to right

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,20 +7,20 @@ export default function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-16 max-w-screen-2xl items-center">
-        <div className="mr-4 flex">
+        <div className="mr-4 flex flex-1">
           <Link href="/" className="mr-6 flex items-center space-x-2">
             <span className="font-headline text-3xl font-bold">Bharat Darshan</span>
           </Link>
         </div>
-        <nav className="flex-1 items-center space-x-4 hidden md:flex">
-            <Link href="/#states" className="text-sm font-medium text-muted-foreground transition-colors hover:text-foreground">
-                Explore
-            </Link>
-             <Link href="/#timeline" className="text-sm font-medium text-muted-foreground transition-colors hover:text-foreground">
-                Timeline
-            </Link>
-        </nav>
-        <div className="flex flex-1 items-center justify-end space-x-2">
+        <div className="flex items-center justify-end space-x-6">
+           <nav className="items-center space-x-4 hidden md:flex">
+                <Link href="/#states" className="text-sm font-medium text-muted-foreground transition-colors hover:text-foreground">
+                    Explore
+                </Link>
+                <Link href="/#timeline" className="text-sm font-medium text-muted-foreground transition-colors hover:text-foreground">
+                    Timeline
+                </Link>
+            </nav>
           <Button variant="ghost" size="icon" asChild>
             <Link href="/search" aria-label="Search">
               <Search className="h-5 w-5" />
