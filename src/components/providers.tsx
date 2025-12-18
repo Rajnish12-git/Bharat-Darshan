a7194f627bb8cd9bbcb9e7ac5dfb2de4d299eec3
Rajnish Gahlauth <rraj81026@gmail.com>
2025-12-18 16:12:17 +0000

    Try fixing this error: `Runtime Error: useFirebase must be used within a

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -2,11 +2,14 @@
 
 import { FavoritesProvider } from '@/context/favorites-context';
 import { APIProvider } from '@vis.gl/react-google-maps';
+import { FirebaseClientProvider } from '@/firebase';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!}>
-      <FavoritesProvider>{children}</FavoritesProvider>
+      <FirebaseClientProvider>
+        <FavoritesProvider>{children}</FavoritesProvider>
+      </FirebaseClientProvider>
     </APIProvider>
   );
 }
