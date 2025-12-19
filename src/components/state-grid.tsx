ff415a04184df877d61a54e817dbc1dbd27c7ff7
Rajnish Gahlauth <rraj81026@gmail.com>
2025-12-19 10:48:43 +0000

    name and description should be bottom of the card

--- src/components/state-grid.tsx
+++ src/components/state-grid.tsx
@@ -90,7 +90,7 @@ export default function StateGrid() {
                       )}
                       <div className="absolute inset-0 bg-gradient-to-t from-black/80 to-transparent" />
                     </div>
-                    <div className="absolute top-0 left-0 p-4 w-full">
+                    <div className="absolute bottom-0 left-0 p-4 w-full">
                        <CardTitle className="font-headline text-2xl text-white drop-shadow-md">
                         {state.name}
                       </CardTitle>
